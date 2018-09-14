18a2a20ce0b590f30153b690c2bd8037e11a6c4f
Folkvir <folkvir@outlook.fr>
2018-09-14 16:05:17 +0200

    offline + send message is ok

--- tests/neighborhood-test.js
+++ tests/neighborhood-test.js
@@ -1,14 +1,17 @@
 const Neighborhood = require('../lib').Neighborhood
 console.log(Neighborhood)
 const wrtc = require('wrtc')
+const assert = require('assert')
 
-describe('Test connection', () => {
+describe('Test connection', function () {
+  this.timeout(2 * 60 * 1000)
   it('connection need to be successfull using a direct connection', async () => {
     const a = new Neighborhood({
       neighborhood: {
         hello: 'world'
       },
       socket: {
+        trickle: true,
         wrtc
       }
     })
@@ -17,10 +20,24 @@ describe('Test connection', () => {
         hello: 'world'
       },
       socket: {
+        trickle: true,
         wrtc
       }
     })
-
     await a.connect(b)
+    assert.strict.equal(a.getNeighboursIds().length, 1)
+    assert.strict.equal(b.getNeighboursIds().length, 1)
+    return new Promise((resolve, reject) => {
+      a.send(b.id, 'miaou').then(() => {
+        console.log('message sent.')
+      }).catch(e => {
+        reject(e)
+      })
+      b.on('receive', (id, data) => {
+        console.log('receive: ', id, data)
+        assert.strict.equal(data, 'miaou')
+        resolve()
+      })
+    })
   })
 })
